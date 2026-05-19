6165ea3c1fa2a0165044f6df936c1ff57e6b9320
Victor Robles <roblesdocarmo@gmail.com>
2026-05-19 17:57:15 -0300

    Página Sobre, página Produtos, adição de imagens e correção de layout

--- loja/views.py
+++ loja/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Produto, Categoria
 # create your views here
+# rota para a página principal - Home
 def home(request):
     produtos = Produto.objects.filter(disponivel = True).order_by('-data_cadastro')[:8]
     categorias = Categoria.objects.all()
@@ -10,3 +11,18 @@ def home(request):
         'categorias': categorias,
     }
     return render(request, 'loja/home.html', contexto)
+
+# rota para a página de Sobre
+def sobre(request):
+    return render(request, 'loja/sobre.html')
+
+# rota para a página de Produtos
+def produtos(request):
+    produtos = Produto.objects.filter(disponivel = True).order_by('nome')
+    categorias = Categoria.objects.all()
+    
+    contexto = {
+        'produtos': produtos,
+        'categorias': categorias
+    }
+    return render(request, 'loja/produtos.html', contexto)
