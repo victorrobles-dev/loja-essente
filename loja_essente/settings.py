119dcafcbc21934b1e7e93ef05f905d0fe050b1f
Victor Robles <roblesdocarmo@gmail.com>
2026-05-20 13:43:58 -0300

    Configuração do Cloudinary para upload de imagens durante o cadastro dos produtos no painel do Django-admin

--- loja_essente/settings.py
+++ loja_essente/settings.py
@@ -102,8 +102,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # cloudinary
-import os
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
